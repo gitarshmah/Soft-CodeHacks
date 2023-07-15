c208fce3fb7c1be9e74b343707a1bd32c60f5f25
Arshmah Saeed <arshmah.saeed27@gmail.com>
2023-07-15 11:59:26 +0530

    done model

--- app.py
+++ app.py
@@ -1,11 +1,39 @@
-from flask import Flask, render_template
-
+from flask import render_template, session, Flask, redirect, url_for
+from flask_wtf import *
+from wtforms import *
+from wtforms.validators import *
+from impinfo import list_of_states, find_crop, find_secity
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'key'
 
-@app.route('/')
-def home():
-    return  render_template('home.html')
+class infoform(FlaskForm):
+    temperature = DecimalField('Temperature: ', validators=[DataRequired()])
+    humidity = DecimalField('Humidity: ', validators=[DataRequired()])
+    ph = DecimalField('Ph: ', validators=[DataRequired()])
+    rainfall = DecimalField('Rainfall(mm): ', validators=[DataRequired()])
+    ChooseState = SelectField("State: ", choices= list_of_states)
+    submit = SubmitField('Submit')
+@app.route('/', methods=['GET', 'POST'])
+def main():
+    form = infoform()
+    if form.validate_on_submit():
+        session['temperature'] = form.temperature.data
+        session['humidity'] = form.humidity.data
+        session['ph'] = form.ph.data
+        session['rainfall'] = form.rainfall.data
+        session['ChooseState'] = form.ChooseState.data
+       
+        y = find_crop(float(form.temperature.data),float(form.humidity.data), float(form.ph.data),float(form.rainfall.data))
+        session['find_crop']=str(y[0])
+        session['find_secity'] = find_secity(str(y[0]),form.ChooseState.data)
+        return redirect(url_for('answer'))
+        
+    return render_template('main.html',form=form)
 
+    
+@app.route('/answer')
+def answer():
+    return render_template('answer.html')
 
-if __name__=='__main__':
+if __name__ == '__main__':
     app.run(debug=True)
